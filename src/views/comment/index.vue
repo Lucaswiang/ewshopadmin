6623aff06da127ca8cb54518b2b5872f09057a28
Lucaswiang <wangliang2008107@163.com>
2022-09-06 21:26:26 +0800

    增加评论管理

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -2,80 +2,103 @@
   <div>
      <div class="h-24 w-full bg-white p-3 mb-6">
         <div>
-            <span class="text-slate-400 pr-1">首页</span> / <span class="pl-1">评论管理</span>
+            <span class="text-slate-400 pr-1">首页</span> / <span class="pl-1">用户列表</span>
             <div class="pt-3 text-xl text-black font-medium">
-              评论管理
+              用户管理
             </div>
         </div>
      </div>
     <div class="content px-4">
+        <div class="bg-white pt-6 pr-6">
+          <n-form
+              ref="formRef"
+              inline
+              :label-width="80"
+              :model="formSearch"
+              label-placement="left"
+          >
+            <n-form-item label="姓名" path="name">
+              <n-input v-model:value="formSearch.name" placeholder="输入姓名" />
+            </n-form-item>
+            <n-form-item label="邮箱" path="email">
+              <n-input v-model:value="formSearch.email" placeholder="请输入邮箱" />
+            </n-form-item>
+
+            <n-form-item class="ml-auto">
+              <n-button class="mr-4" attr-type="button" @click="searchReload">
+                重置
+              </n-button>
+              <n-button type="info" attr-type="button" @click="searchSubmit">
+                搜索
+              </n-button>
+            </n-form-item>
+          </n-form>
+        </div>
         <div class="mt-4 bg-white">
           <div class="text-xl px-6 py-4 flex ">
-            <span>轮播图列表</span>
+            <span>用户列表</span>
             <span class="ml-auto"><NButton type="info" @click="showModal = true" >+ 新建</NButton></span>
           </div>
-          <div class="px-6">
+          <div class="px-4">
+
             <n-data-table
                 :columns="columns"
                 :data="data"
                 :pagination="pagination"
                 :bordered="false"
+                row-class-name="rowclass"
             />
             <div class="p-4 flex justify-end pr-10">
               <n-pagination v-model:page="page" @update:page="updatePage" :page-count="totalPages" />
             </div>
           </div>
         </div>
-        <AddSlide :showModal="showModal" @checkShowModal="checkShowModal" @reloadTable="reload"></AddSlide>
-        <EditSlide v-if="showEditModal"  :slide_id="slide_id" :showModal="showEditModal" @checkShowModal="checkEditModal" @reloadTable="reload"></EditSlide>
+        <AddUser :showModal="showModal" @checkShowModal="checkShowModal" @reloadTable="reload"></AddUser>
+        <EditUser v-if="showEditModal"  :user_id="user_id" :showModal="showEditModal" @checkShowModal="checkEditModal" @reloadTable="reload"></EditUser>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { h,ref,onMounted } from 'vue'
-import { NButton, useMessage,NImage,NSwitch,useLoadingBar } from 'naive-ui'
-import AddSlide from './components/AddSlide.vue'
-import EditSlide from './components/EditSlide.vue'
-import { slides } from '@/api/slide'
+import { NButton, useMessage,NAvatar,NSwitch,useLoadingBar } from 'naive-ui'
+import AddUser from './components/AddUser.vue'
+import EditUser from './components/EditUser.vue'
+import { users,lockUser } from '@/api/users'
 const page = ref(1)
 const message = useMessage()
 const data = ref([])
 const totalPages = ref(0)
 const columns = [
   {
-    title: '轮播图片',
-    key: 'img_url',
+    title: '头像',
+    key: 'avatar_url',
     render (row) {
-      return h(NImage,{round:false,src:row.img_url,class:'h-12'})
+      return h(NAvatar,{round:true,src:row.avatar_url,size:'medium'})
     }
   },
   {
-    title: '标题',
-    key: 'title'
+    title: '姓名',
+    key: 'name'
   },
   {
-    title: '跳转链接',
-    key: 'url'
+    title: '邮箱',
+    key: 'email'
   },
   {
     title: '是否禁用',
-    key: 'status',
+    key: 'is_locked',
     render(row){
       return h(NSwitch,{
-        size:'small',
+        size:'medium',
         color:'#1890ff',
         activeColor:'#52c41a',
         inactiveColor:'#d9d9d9',
         activeValue:1,
         inactiveValue:0,
-        value:row.status == 1 ? false : true,
+        value:row.is_locked == 1 ? false : true,
       })
     }
-  },
-  {
-    title: '排序',
-    key: 'seq',
   },
   {
     title: '创建时间',
@@ -90,20 +113,23 @@ const columns = [
         color:'#1890ff',
         strong:true,
         onClick:()=>{
-          slide_id.value = row.id
+           user_id.value = row.id
             showEditModal.value = true
         }
       },'编辑')
     }}
 ]
 const pagination = ref(false as const)
-
+const formSearch = ref({
+  name:'',
+  email:''
+})
 // 添加模态框显示状态
 const showModal = ref(false)
 // 编辑模态框
 const showEditModal = ref(false)
 
-const slide_id = ref('')
+const user_id = ref('')
 
 const checkEditModal = (show:boolean) => {
   showEditModal.value = show
@@ -111,21 +137,36 @@ const checkEditModal = (show:boolean) => {
 const loadingBar = useLoadingBar()
 
 onMounted(()=>{
-  getSlidesList({})
+  getUserList({})
 })
 const updatePage = (pageNum) => {
-  getSlidesList({
-    current:pageNum
+  getUserList({
+    current:pageNum,
+    name:formSearch.value.name,
+    email:formSearch.value.email
   })
 }
-
-
-const getSlidesList = (params) =>{
+const searchSubmit = (e) =>{
+  e.preventDefault()
+  getUserList({
+    name:formSearch.value.name,
+    email:formSearch.value.email,
+    current:1
+  })
+}
+const searchReload = ()=>{
+  getUserList({})
+  formSearch.value = {
+    name:'',
+    email:''
+  }
+}
+const getUserList = (params) =>{
   loadingBar.start()
-  slides(params).then(slides =>{
-    data.value = slides.data
-    totalPages.value = slides.meta.pagination.total_pages
-    page.value = slides.meta.pagination.current_page
+  users(params).then(users =>{
+    data.value = users.data
+    totalPages.value = users.meta.pagination.total_pages
+    page.value = users.meta.pagination.current_page
     loadingBar.finish()
   }).catch(err=>{
     loadingBar.error()
@@ -135,12 +176,19 @@ const checkShowModal = (status)=>{
   showModal.value = status
 }
 const reload = ()=>{
-  getSlidesList({
-    current:page.value
+  getUserList({
+    current:page.value,
+    name:formSearch.value.name,
+    email:formSearch.value.email
   })
 }
 </script>
 
-<style scoped>
-
+<style lang="less">
+  .rowclass{
+    height: 65px;
+    .n-data-table-td{
+      font-size: 14px;
+    }
+  }
 </style>
